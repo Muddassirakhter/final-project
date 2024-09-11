6f04b9f86b98b39b2b9abd7f1a8e4dab3987ff44
Muddassirakhter <137832004+Muddassirakhter@users.noreply.github.com>
2024-09-11 16:54:10 +0500

    Add files via upload

--- weather/app.js
+++ weather/app.js
@@ -29,3 +29,35 @@ async function getWeather(event) {
     }
 
 }
+
+function updateBackground() {
+    // Get the current hour
+    const now = new Date();
+    const hours = now.getHours();
+
+    // Determine the background image based on the hour
+    let backgroundImage;
+
+    switch (true) {
+        case (hours >= 6 && hours < 12):
+            // Morning
+            backgroundImage = 'url("morning1.jpg")';
+            break;
+        case (hours >= 12 && hours < 18):
+            // Afternoon
+            backgroundImage = 'url("afternoon3.jpg")';
+            break;
+        case (hours >= 18 && hours < 20):
+            // Evening
+            backgroundImage = 'url("evening.webp")';
+            break;
+        default:
+            // Night
+            backgroundImage = 'url("night2.jpg")';
+            break;
+    }
+
+    // Apply the background image to the body
+    document.body.style.backgroundImage = backgroundImage;
+}
+updateBackground();
